0753e945f2512aa676b18009328b97b6599fe638
ce sun <cesun@cedeMacBook-Pro.local>
2017-03-31 17:53:19 +0800

    finish layout and routes

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','help')
+@section('content')
+  <h1>-help pages-</h1>
+@stop
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title',"signup for new users")
+
+@section('content')
+<h1>singup</h1>
+@stop
